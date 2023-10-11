0371cd74ef52331ccdb6ed1c62509d32586ac77b
Onibless <ocheiblessyn@gmail.com>
2023-10-11 16:00:00 -0700

    gcc

---
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -0,0 +1,14 @@
+#include "search_algos.h"
+/**
+ *linear_skip - function that searches for a value
+ *      in a sorted array of integers using the Interpolation
+ *      search algorithm
+ *@list: array to search
+ *@value:  the value to search for
+ *Return: return the first index where value is located
+ *      If value is not present in array or if array is NULL,
+ *      returns -1
+ */
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+}
